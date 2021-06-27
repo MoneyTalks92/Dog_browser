8a297159c72f00a5079bfdea6660eff0b18e9dba
Ferenc Bujtas <anothercp3@gmail.com>
2021-06-27 12:13:32 +0200

    hibák javítása

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import './css/style.css';
+import './index.html';
+import ListBreeds from './listBreedsComponent/listBreedsComponent';
+import SearchImage from './searchImageComponent/searchImageComponent';
+
+new SearchImage();
+
+new ListBreeds();
